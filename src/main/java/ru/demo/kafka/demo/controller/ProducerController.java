3fa26cc03a8666d6d8d562b0d7850b33a48120dc
Maksim Vinogradov <maxvgrad@gmail.com>
2019-03-03 01:37:30 +0300

    Add ProducerController and ProducerService

--- src/main/java/ru/demo/kafka/demo/controller/ProducerController.java
+++ src/main/java/ru/demo/kafka/demo/controller/ProducerController.java
@@ -3,33 +3,63 @@ package ru.demo.kafka.demo.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.demo.kafka.demo.DemoApplication;
-import ru.demo.kafka.demo.dto.ProducerRecord;
+import ru.demo.kafka.demo.dto.DocumentDto;
 import ru.demo.kafka.demo.service.ProducerService;
 
+import java.util.Map;
+
 @Slf4j
-@RestController(DemoApplication.APP_ROOT_PATH + "/producer")
+@RestController
 @RequiredArgsConstructor
+@RequestMapping(DemoApplication.APP_ROOT_PATH + "/producer")
 public class ProducerController {
 
     private final ProducerService service;
 
-    @PostMapping("/configure")
-    public ResponseEntity configure(@RequestBody String bootstrapServers) {
-        log.debug("#configure: bootstrapServers({})", bootstrapServers);
-        service.configure(bootstrapServers);
-        return ResponseEntity.ok().build();
+    @GetMapping("/}")
+    public Map<String, Object> get() {
+        log.debug("#get:");
+        return service.get();
     }
 
-    @PostMapping("/send")
-    public ResponseEntity send(@RequestBody ProducerRecord record) {
-        log.debug("#send: {}", record);
-
-        return ResponseEntity.ok().build();
+    @GetMapping("/{key}")
+    public Map.Entry<String, Object> getValue(@PathVariable String key) {
+        log.debug("#getValue: key:{}", key);
+        return service.getValue(key);
     }
 
+    @PutMapping("/")
+    public Map<String, Object> put(Map<String, String> properties) {
+        log.debug("#put: {}", properties);
+        return service.put(properties);
+    }
+
+    @PatchMapping("/")
+    public Map<String, Object> patch(String kafkaBootStrapServers, String groupId) {
+        log.debug("#patch: kafka-bootstrap-servers: {}, groupId: {}, topic: {}", kafkaBootStrapServers, groupId);
+        return null;
+    }
+
+    @DeleteMapping("/")
+    public Map.Entry<String, String> remove(String key) {
+        log.debug("#remove: key: {}", key);
+
+        return null;
+    }
+
+    @PostMapping("/send/{topic}")
+    public ResponseEntity send(@PathVariable String topic, @RequestBody DocumentDto document) {
+        log.debug("#send: topic: {}, document: {}", topic, document);
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/ru/demo/kafka/demo/service/ProducerService.java
+++ src/main/java/ru/demo/kafka/demo/service/ProducerService.java
@@ -2,18 +2,61 @@ package ru.demo.kafka.demo.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.stereotype.Service;
+import ru.demo.kafka.demo.bean.ProducerKafka;
+import ru.demo.kafka.demo.dto.DocumentDto;
+
+import java.util.Map;
 
 @Slf4j
-@Service()
+@Service
 @RequiredArgsConstructor
 public class ProducerService {
 
-    private KafkaProducer<String, String> producer;
+    private final ProducerKafka producer;
 
-    public void configure(String bootStrapServers) {
 
+    public Map<String, Object> get() {
+        return producer.getProperties();
+    }
+
+    public Map.Entry<String, Object> getValue(String key) {
+        return producer.getProperty(key);
+    }
+
+    public Map<String, Object> put(Map<String, ?> properties) {
+        producer.configure(properties);
+        return producer.getProperties();
+    }
+
+    public Map<String, Object> patch(String kafkaBootStrapServers, String groupId) {
+        producer.configure(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBootStrapServers);
+        producer.configure(ProducerConfig.CLIENT_ID_CONFIG, groupId);
+
+        return producer.getProperties();
+    }
+
+    public Map.Entry<String, Object> remove(String key) {
+
+        Map.Entry<String, Object> entry = producer.getProperty(key);
+
+        if (entry.getValue() != null) {
+            Map<String, Object> properties = producer.getProperties();
+            properties.remove(key);
+
+            producer.configure(properties);
+        }
+        return entry;
+    }
+
+    public void send(String topic, String key, DocumentDto document) {
+
+        ProducerRecord<String, DocumentDto> record = new ProducerRecord<String, DocumentDto>(topic, key, document);
+
+        producer.send(record);
+        return;
     }
 
 }
